25a43cd482b69c82160687779b90553cfd4d3498
Jessica-GIRAUD <jessica.giraud@strateg.in>
2022-04-23 14:19:35 +0200

    cors

--- index.js
+++ index.js
@@ -15,6 +15,11 @@ app.use(express.json());
 // Parse URL-encoded bodies (as send by HTML forms)
 app.use(express.urlencoded({ extended: false }));
 
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  next();
+});
+
 app.use(
   cors({
     origin: ["https://pizza-kika.netlify.app"],
