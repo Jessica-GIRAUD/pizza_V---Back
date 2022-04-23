711513d600b9f50be513e90d34a61f2599db12e9
Jessica-GIRAUD <jessica.giraud@strateg.in>
2022-04-23 15:23:49 +0200

    createPool

--- db.js
+++ db.js
@@ -10,29 +10,12 @@ const config = {
   password: DB_PASSWORD,
   database: DB_NAME,
 };
+const connection = mysql.createPool(config);
 
-var connection;
-
-function handleDisconnect() {
-  connection = mysql.createConnection(config);
-
-  connection.connect(function (err) {
-    if (err) {
-      console.log("error when connecting to db:", err);
-      setTimeout(handleDisconnect, 2000);
-    } else {
-      console.log("connection is successfull");
-    }
-  });
-  connection.on("error", function (err) {
-    console.log("db error", err);
-    if (err.code === "PROTOCOL_CONNECTION_LOST") {
-      handleDisconnect();
-    } else {
-      throw err;
-    }
-  });
-}
-handleDisconnect();
+connection.connect((error) => {
+  if (error) {
+    console.log(error);
+  } else console.log("Database connected");
+});
 
 module.exports = connection;
