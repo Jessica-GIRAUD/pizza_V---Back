7622113fc4082f164fe062ea1a3bd2fbd540e061
Jessica-GIRAUD <jessica.giraud@strateg.in>
2022-04-23 16:19:26 +0200

    test

--- index.js
+++ index.js
@@ -10,14 +10,13 @@ const { verifyJWT } = require("./middlewares/jwt");
 
 const { PORT } = process.env;
 
-app.use(function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", "https://pizza-kika.netlify.app"); // update to match the domain you will make the request from
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept"
-  );
-  next();
-});
+const corsOptions = {
+  origin: "*",
+  credentials: true, //access-control-allow-credentials:true
+  optionSuccessStatus: 200,
+};
+
+app.use(cors(corsOptions)); // Use this after the variable declaration
 
 app.use(express.json());
 
