dda282ef8df6c5cce02ba521e86b365a8edd183b
Jessica-GIRAUD <jessica.giraud@strateg.in>
2022-05-14 23:55:51 +0200

    cors

--- index.js
+++ index.js
@@ -26,6 +26,15 @@ app.use(express.urlencoded({ extended: false }));
 // middleware for cookies
 app.use(cookieParser());
 
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
+
 // routes
 
 app.use("/", require("./routes/globals"));
